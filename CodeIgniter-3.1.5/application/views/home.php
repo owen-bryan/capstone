f82a27cd1fe6b0f517a2945ea88b7524961f4ddd
owen-bryan <odog654@gmail.com>
2017-09-18 01:59:05 -0400

    2017/09/18 - 2
    Added beginnings of advanced options for searching.

--- CodeIgniter-3.1.5/application/views/home.php
+++ CodeIgniter-3.1.5/application/views/home.php
@@ -4,7 +4,11 @@
 		<h1>Card Trader</h1>
 			<form action="/search" method="post">
 				<input type="text" placeholder="Enter search here"/>
-				<input type="submit" value="Search"/>
+				<input type="submit" value="Search"/><br/>
+				<a href="#" onclick="show_advanced();">Click here for advanced options</a><br/>
+				<div id="advanced options">
+				
+				</div>
 			</form>
 	</div>
 </div>
