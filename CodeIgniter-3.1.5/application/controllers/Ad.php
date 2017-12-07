5aa7d2df96892258dda893896b40e940ab65d7fd
owen <odog654@gmail.com>
2017-12-07 04:18:48 -0500

    2017/12/07 - 1

--- CodeIgniter-3.1.5/application/controllers/Ad.php
+++ CodeIgniter-3.1.5/application/controllers/Ad.php
@@ -30,7 +30,7 @@ class Ad extends CI_Controller
 	
 	private function get_ad($id)
 	{
-		$this->db->select('ADS.ad_id, item_description, ad_title, item_condition, item_price, post_date, user_name, brand_name, image_location, city, province, category_name, USERS.user_name');
+		$this->db->select('ADS.ad_id, item_description, views, ad_title, item_condition, item_price, post_date, user_name, brand_name, image_location, address, city, province, category_name, USERS.id, USERS.user_name');
 		$this->db->from('ADS');
 		$this->db->join('USERS', 'ADS.user_id = USERS.id');
 		$this->db->join('BRANDS', 'ADS.brand_id = BRANDS.brand_id');
--- CodeIgniter-3.1.5/application/views/ad.php
+++ CodeIgniter-3.1.5/application/views/ad.php
@@ -1,16 +1,49 @@
 <div class="container-fluid col-md-8 col-md-offset-2">
 	<div class="container col-md-12 panel panel-default">
-		<h2><?= $details['ad_title']?></h2>
+		<div class="row">
+			<div class="col-md-8">
+				<h3><?= $details['ad_title']?></h2>
+			</div>
+			<div class="col-md-4 text-right">
+				<h3>Sold by: <?= $details['user_name'] ?></h3>
+			</div>
+		</div>
 		<hr/>
 		<div class="row panel-body">
-			<div class="container col-md-3">
-				<img src="<?= base_url() . "uploads/" . $details['image_location'] ?>" class="img-responsive"/> 
+			<div class= "row" >
+				<div class="container col-md-3">
+					<img src="<?= base_url() . "uploads/" . $details['image_location'] ?>" class="img-responsive"/> 
+				</div>
+				<div class="container col-md-7">
+					<div class="row">
+						<p>Price: $<?= $details['item_price'] ?></p>
+					</div>
+					<div class="row">
+						<p>Location: <?= ucfirst($details['address']) . ", " . ucfirst($details['city']) . ", " . ucfirst($details['province']) ?></p>
+					</div>
+					<div class="row">
+						<p>Date posted: <?= date("d/m/Y", strtotime($details['post_date'])) ?></p>
+					</div>
+					<div class="row">
+						<p>Condition: <?= $details['item_condition'] ?></p>
+					</div>
+					<div class="row">
+						<p>Description: <?= $details['item_description'] ?></p>
+					</div>
+				</div>
+				<div class="container col-md-2">
+					<a href="<?= base_url() . "index.php?c=message&m=new_message&ad_id=" . $details['ad_id'] . "&user=" . $details['id'] ?>" class="btn btn-primary">Contact this user</a>
+				</div>
 			</div>
-			<div class="container col-md-5">
-			
-			</div>
-			<div class="container col-md-4">
-			
+			<div class="row">
+				<div class="text-left col-md-9">
+					<a href="<?= base_url() . "index.php?/c=ad&m=report&user=" .$details[id] ?>">Report this user</a>
+				</div>
+				<div class="text-right col-md-3">
+					<div class="">
+						<p>Views: <?= $details['views'] ?></p>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
